6c61e63877e5482a3fb985f0b7380f176e3eab36
Albert Hwang <hwang.al@gmail.com>
2016-10-28 17:16:24 -0400

    first commit

---
+++ Assets/Learning Scenes/Move To Keys/MoveToKeys.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToKeys : MonoBehaviour {
+	void Update () {
+		if (Input.GetKey(KeyCode.Space)) {
+			hover ();
+		}
+		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+			move(Vector3.right);
+		}
+		if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
+			move(Vector3.forward);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+			move(Vector3.left);
+		}
+		if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
+			move(Vector3.back);
+		}
+	}
+
+	void hover() {
+		GetComponent<Rigidbody> ().AddForce (Vector3.up * 0.5f, ForceMode.Impulse);
+	}
+
+	void move(Vector3 moveDirection) {
+		transform.position = transform.position + moveDirection / 10.0f;
+	}
+}
